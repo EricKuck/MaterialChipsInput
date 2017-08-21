cd2bdf2785b0a591ed36cab80937ee3f060dfb09
Eric Kuck <eric@bluelinelabs.com>
2017-08-21 17:56:14 -0500

    Update ChipsInput.java

--- library/src/main/java/com/pchmn/materialchips/ChipsInput.java
+++ library/src/main/java/com/pchmn/materialchips/ChipsInput.java
@@ -11,6 +11,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 
 import com.beloo.widget.chipslayoutmanager.ChipsLayoutManager;
